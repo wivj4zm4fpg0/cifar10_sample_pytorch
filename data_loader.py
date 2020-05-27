da68b3c76974a0bb361a98ad843884a9b519a141
wivj4zm4fpg0 <nzswlrkad8cw@gmail.com>
2020-05-27 20:38:26 +0900

    update

--- data_loader.py
+++ data_loader.py
@@ -1,9 +1,12 @@
 import os
 
+import matplotlib.pyplot as plt
+import numpy as np
 import torch
 from PIL import Image
 from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms
+from torchvision.utils import make_grid
 
 
 class dataSet(Dataset):
@@ -17,7 +20,7 @@ class dataSet(Dataset):
             self.pre_processing = pre_processing
         else:
             self.pre_processing = transforms.Compose([
-                transforms.RandomHorizontalFlip,  # ランダムで左右回転
+                transforms.RandomHorizontalFlip(),  # ランダムで左右回転
                 transforms.ToTensor(),  # Tensor型へ変換
                 transforms.Normalize((0, 0, 0), (1, 1, 1))  # 画素値が0と1の間になるように正規化
             ])
@@ -28,15 +31,19 @@ class dataSet(Dataset):
             class_path = os.path.join(root_path, class_name)
             for image_name in os.listdir(class_path):
                 image_path = os.path.join(class_path, image_name)
-                img = self.pre_processing(Image.open(image_path))  # PIL画像を読み込みtransformsで前処理を行う
                 # クラス分類のラベルは猫(正解とする)、犬、トラ->(1, 0, 0)となる
                 target = torch.zeros(class_len)
                 target[i] = 1
-                self.data_list.append((img, target))
+                self.data_list.append((image_path, target))
 
     # イテレートするときに実行されるメソッド．ここをオーバーライドする必要がある．
     def __getitem__(self, index: int) -> tuple:
-        return self.data_list[index]  # 入力画像とそのラベルをタプルとして返す
+        img = self.pre_processing(Image.open(self.data_list[index][0]))  # PIL画像を読み込みtransformsで前処理を行う
+        label = self.data_list[index][1]
+        return img, label  # 入力画像とそのラベルをタプルとして返す
+
+    def __len__(self):
+        return len(self.data_list)
 
 
 if __name__ == '__main__':
@@ -52,5 +59,16 @@ if __name__ == '__main__':
         dataSet(path=args.dataset_path, subset=args.subset),
         batch_size=1, shuffle=False
     )
+
+
+    def image_show(img):  # 画像を表示
+        img = img / 2 + 0.5  # unnormalize
+        np_img = img.numpy()
+        plt.imshow(np.transpose(np_img, (1, 2, 0)))
+        plt.show()
+        input()
+
+
     for input_image, input_label in data_loader:
         print(input_label)
+        image_show(make_grid(input_image))
