4602008e9c04f87a12712dec6631a26479f4494e
wivj4zm4fpg0 <nzswlrkad8cw@gmail.com>
2020-06-01 16:34:46 +0900

    update

--- CNN_LSTM_Model.py
+++ CNN_LSTM_Model.py
@@ -24,17 +24,28 @@ class CNN_LSTM(nn.Module):
 
         self.fc = nn.Linear(4096, class_num)
 
-    # xの形は(RNNへの入力数, バッチサイズ, チャンネル数, 解像度, 解像度)である必要がある
+    # xの形は(バッチサイズ, RNNへの入力数, チャンネル数, 解像度, 解像度)の5次元配列である必要がある
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        x = torch.stack([torch.flatten(self.resnet18(x[i]), 1) for i in range(len(x))])
+
+        batch_size = x.shape[0]
+        sequence_length = x.shape[1]
+
+        # (バッチサイズ x RNNへの入力数, チャンネル数, 解像度, 解像度)の4次元配列に変換する
+        x = x.view(batch_size * sequence_length, x.shape[2], x.shape[3], x.shape[4])
+        x = self.resnet18(x)
+        x = x.view(sequence_length, batch_size, -1)
+
+        # x = torch.stack([torch.flatten(self.resnet18(x[i]), 1) for i in range(batch_size)])
+        # x = x.permute(1, 0, 2)
+
         x = self.lstm1(x)[0]
         x = self.lstm2(x)[0]
-        x = self.fc(x)
-        return x  # (RNNの出力データ数{入力データ数と同じ}, バッチサイズ, クラス数}の形になる
+        x = self.fc(x)  # (シーケンスの長さ, バッチサイズ, クラス数)が出力される
+        return x.permute(1, 0, 2)  # (バッチサイズ, シーケンスの長さ, クラス数}の形にする
 
 
 if __name__ == '__main__':
     model = CNN_LSTM()
-    input = torch.randn(4, 2, 3, 256, 256)
+    input = torch.randn(2, 4, 3, 256, 256)
     output = model(input)
     print(f'{output.shape=}')
