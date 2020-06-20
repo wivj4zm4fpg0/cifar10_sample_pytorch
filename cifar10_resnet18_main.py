43a34fcd7703b8e421b20df9efbe33f65385567d
wivj4zm4fpg0 <nzswlrkad8cw@gmail.com>
2020-06-20 22:34:30 +0900

    update

--- cifar10_resnet18_main.py
+++ cifar10_resnet18_main.py
@@ -27,9 +27,9 @@ log_train_path = os.path.join(args.output_dir, 'log_train.csv')
 log_test_path = os.path.join(args.output_dir, 'log_test.csv')
 model_save_path = os.path.join(args.output_dir, 'model.pth')
 epoch_num = args.epoch_num
+os.makedirs(args.output_dir, exist_ok=True)
 json.dump(vars(args), open(os.path.join(args.output_dir, 'args.jsons'), mode='w'),
           ensure_ascii=False, indent=4, sort_keys=True, separators=(',', ': '))
-os.makedirs(args.output_dir, exist_ok=True)
 
 # データセットを読み込む
 train_loader = DataLoader(
